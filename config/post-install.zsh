6f951324be83052320a5af07b51acf36ab8d7f66
Chris Thomas <xiotox@gmail.com>
2023-07-01 12:03:46 -0400

    fix install scripts

--- config/post-install.zsh
+++ config/post-install.zsh
@@ -1,8 +1,8 @@
 # This gets sourced after 'antidote load'
 
 # Keybindings for zsh-users/zsh-history-substring-search
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+# bindkey '^[[A' history-substring-search-up
+# bindkey '^[[B' history-substring-search-down
 
 # autocompletion using arrow keys (based on history)
 # bindkey '\e[A' history-search-backward
--- install-deps.sh
+++ install-deps.sh
@@ -10,20 +10,20 @@ if [ ! `command -v volta` ]; then
   export PATH="$VOLTA_HOME/bin:$PATH"
 fi
 
-# install node
-[ ! `command -v node` ] && \
-  $HOME/.volta/bin/volta install node
+# install node if needed
+node --version 2>/dev/null || $HOME/.volta/bin/volta install node@16
 
 if [ IS_OSX ]; then
 
   # Install Homebrew
-  if [ ! `command -v brew` ]; then
+  if ! command -v brew &> /dev/null; then
     echo "Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    export PATH="/opt/homebrew/bin:$PATH"
   fi
 
   # Install jq
-  if [ ! `command -v jq` ]; then
+  if ! command -v jq &> /dev/null; then
     echo "Installing 'jq' from homebrew."
     brew install jq
   fi
